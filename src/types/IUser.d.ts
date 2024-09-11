9807da3f7aed142a30879ac76aedd10417a4078b
LauraMessager <150282637+LauraMessager@users.noreply.github.com>
2024-09-11 15:40:02 +0200

    modif IUser

--- src/types/IUser.d.ts
+++ src/types/IUser.d.ts
@@ -1,11 +1,7 @@
 import { Document, Types } from "mongoose";
-import { IComment } from "./IComment";
-import { IPost } from "./IPost";
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  posts?: IPost[];
-  comments?: IComment[];
 }
